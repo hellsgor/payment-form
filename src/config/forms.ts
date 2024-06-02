0d91e0f4500e6d31b3cfd085e53e78b7543a124d
Igor Storchevoy <hellsgor@gmail.com>
2024-06-02 14:55:32 +0400

    feat(main.js): параметры формы вынесены в отдельный конфиг

---
+++ src/config/forms.ts
@@ -0,0 +1,56 @@
+import { Config } from '../types/configs';
+import { FormProps } from '../components/form/Form.types';
+
+export const formsConfig: Config<FormProps> = {
+  paymentForm: {
+    form: {
+      name: 'payment',
+    },
+    title: 'Payment form',
+    controls: [
+      {
+        type: 'number',
+        name: 'number',
+        required: true,
+        autocomplete: 'cc-number',
+        placeholder: 'XXXX-XXXX-XXXX-XXXX',
+        labelText: 'Card number',
+        inputmode: 'numeric',
+      },
+      {
+        type: 'text',
+        name: 'expiration-date',
+        required: true,
+        autocomplete: 'cc-exp',
+        placeholder: 'MM/YY',
+        labelText: 'Expiration Date',
+        inputmode: 'numeric',
+      },
+      {
+        type: 'number',
+        name: 'code',
+        required: true,
+        autocomplete: 'cc-csc',
+        placeholder: 'XXX',
+        labelText: 'CVC/CVV',
+        inputmode: 'numeric',
+      },
+      {
+        type: 'email',
+        name: 'email',
+        required: true,
+        autocomplete: 'email',
+        placeholder: 'Email for check',
+        labelText: 'Email',
+      },
+      {
+        name: 'payment-amount',
+        required: true,
+        hidden: true,
+      },
+    ],
+    button: {
+      text: 'Pay',
+    },
+  },
+};
--- src/main.ts
+++ src/main.ts
@@ -2,59 +2,13 @@ import './styles/index.scss';
 import { el, mount } from 'redom';
 import { FormComponent } from './components/form/Form';
 import { IForm } from './components/form/Form.types';
+import { formsConfig } from './config/forms';
 
+let paymentForm: IForm | null = null;
 const app = el('#app');
-const paymentForm: IForm = new FormComponent({
-  form: {
-    name: 'payment',
-  },
-  title: 'Payment form',
-  controls: [
-    {
-      type: 'number',
-      name: 'number',
-      required: true,
-      autocomplete: 'cc-number',
-      placeholder: 'XXXX-XXXX-XXXX-XXXX',
-      labelText: 'Card number',
-      inputmode: 'numeric',
-    },
-    {
-      type: 'text',
-      name: 'expiration-date',
-      required: true,
-      autocomplete: 'cc-exp',
-      placeholder: 'MM/YY',
-      labelText: 'Expiration Date',
-      inputmode: 'numeric',
-    },
-    {
-      type: 'number',
-      name: 'code',
-      required: true,
-      autocomplete: 'cc-csc',
-      placeholder: 'XXX',
-      labelText: 'CVC/CVV',
-      inputmode: 'numeric',
-    },
-    {
-      type: 'email',
-      name: 'email',
-      required: true,
-      autocomplete: 'email',
-      placeholder: 'Email for check',
-      labelText: 'Email',
-    },
-    {
-      name: 'payment-amount',
-      required: true,
-      hidden: true,
-    },
-  ],
-  button: {
-    text: 'Pay',
-  },
-});
 
-mount(app, paymentForm.form);
-mount(document.body, app);
+if (formsConfig['paymentForm']) {
+  paymentForm = new FormComponent(formsConfig['paymentForm']);
+  mount(app, paymentForm.form);
+  mount(document.body, app);
+}
