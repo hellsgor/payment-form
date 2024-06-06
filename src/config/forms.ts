20e2f9f5350163cff6e915d18c00b30cf7e8f54f
Igor Storchevoy <hellsgor@gmail.com>
2024-06-07 00:33:45 +0400

    feat(forms): параметры масок вынесены в константу

--- src/config/forms.ts
+++ src/config/forms.ts
@@ -1,6 +1,6 @@
-import IMask, { FactoryArg } from 'imask';
 import { FormProps } from '../components/Form/Form.types';
 import { Config } from '../types/configs';
+import { maskConfigs } from './masks';
 
 export const formsConfig: Config<FormProps> = {
   paymentForm: {
@@ -14,12 +14,10 @@ export const formsConfig: Config<FormProps> = {
         name: 'number',
         required: true,
         autocomplete: 'cc-number',
-        placeholder: '0000 0000 0000 0000',
+        placeholder: '0000 0000 0000 0000 00',
         labelText: 'Card number',
         inputmode: 'numeric',
-        mask: {
-          mask: '0000 0000 0000 0000 [00]',
-        } as FactoryArg,
+        mask: maskConfigs.get('card-number'),
       },
       {
         type: 'text',
@@ -29,41 +27,7 @@ export const formsConfig: Config<FormProps> = {
         placeholder: 'mm/yy',
         labelText: 'Expiration Date',
         inputmode: 'numeric',
-        mask: {
-          mask: Date,
-          pattern: 'MM{/}YY',
-          blocks: {
-            MM: {
-              mask: IMask.MaskedRange,
-              from: 1,
-              to: 12,
-              maxLength: 2,
-            },
-            YY: {
-              mask: IMask.MaskedRange,
-              from: 0,
-              to: 99,
-              maxLength: 2,
-            },
-          },
-          format: (date: Date): string => {
-            const month = date.getMonth() + 1;
-            const year = date.getFullYear() % 100;
-            return [
-              month < 10 ? '0' + month : month,
-              year < 10 ? '0' + year : year,
-            ].join('/');
-          },
-          parse: (str: string): Date => {
-            const [month, year] = str
-              .split('/')
-              .map((item) => parseInt(item, 10));
-            return new Date(2000 + year!, month! - 1);
-          },
-          autofix: true,
-          lazy: true,
-          overwrite: true,
-        } as FactoryArg,
+        mask: maskConfigs.get('card-date'),
       },
       {
         type: 'text',
@@ -73,9 +37,7 @@ export const formsConfig: Config<FormProps> = {
         placeholder: '000',
         labelText: 'CVC/CVV',
         inputmode: 'numeric',
-        mask: {
-          mask: '000',
-        } as FactoryArg,
+        mask: maskConfigs.get('card-security-code'),
       },
       {
         type: 'email',
@@ -84,7 +46,6 @@ export const formsConfig: Config<FormProps> = {
         autocomplete: 'email',
         placeholder: 'Email for check',
         labelText: 'Email',
-        // mask: '',
       },
     ],
     button: {
